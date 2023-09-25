7eacbbc5d973dc6196c0bce70dc9b1e49ea083c8
Herick Bittencourt <herick@edu.univali.br>
2023-09-25 11:22:08 -0300

    Atualizado codigo.asm

--- main/main/codigo.asm
+++ main/main/codigo.asm
@@ -1,23 +1,34 @@
-00000000010100000000010000010011 addi s0, zero, 5 (4) | addi s0, zero, 5 (5) 
-00000000101000000000010010010011 addi s1, zero, 10 (4) | addi s1, zero, 10 (5) 
-00000000100000101000100001100011 beq  t0, s0, fim (3) | beq  t0, s0, fim (5)
-00000000010101001000010010110011 add  s1, s1, t0 (4) | add  s1, s1, t0 (5)
-00000000000100101000001010010011 addi t0, t0, 1 (4) | addi t0, t0, 1 (5) 
-11111111010111111111000001101111 jal  zero, for (3) | jal  zero, for (5) 
-00000000100000101000100001100011 beq  t0, s0, fim (3) | beq  t0, s0, fim (5)
-00000000010101001000010010110011 add  s1, s1, t0 (4) | add  s1, s1, t0 (5)
-00000000000100101000001010010011 addi t0, t0, 1 (4) | addi t0, t0, 1 (5) 
-11111111010111111111000001101111 jal  zero, for (3) | jal  zero, for (5) 
-00000000100000101000100001100011 beq  t0, s0, fim (3) | beq  t0, s0, fim (5)
-00000000010101001000010010110011 add  s1, s1, t0 (4) | add  s1, s1, t0 (5)
-00000000000100101000001010010011 addi t0, t0, 1 (4) | addi t0, t0, 1 (5) 
-11111111010111111111000001101111 jal  zero, for (3) | jal  zero, for (5) 
-00000000100000101000100001100011 beq  t0, s0, fim (3) | beq  t0, s0, fim (5)
-00000000010101001000010010110011 add  s1, s1, t0 (4) | add  s1, s1, t0 (5)
-00000000000100101000001010010011 addi t0, t0, 1 (4) | addi t0, t0, 1 (5) 
-11111111010111111111000001101111 jal  zero, for (3) | jal  zero, for (5) 
-00000000100000101000100001100011 beq  t0, s0, fim (3) | beq  t0, s0, fim (5)
-00000000010101001000010010110011 add  s1, s1, t0 (4) | add  s1, s1, t0 (5)
-00000000000100101000001010010011 addi t0, t0, 1 (4) | addi t0, t0, 1 (5) 
-11111111010111111111000001101111 jal  zero, for (3) | jal  zero, for (5) 
-00000000100000101000100001100011 beq  t0, s0, fim (3) | beq  t0, s0, fim (5)
+.data
+	v: .word 1,2,3,4,5,6,7,8
+.text
+	la 		s0, v
+	addi	s7, zero, 7 #tamanho vetor -1 = 7
+
+## ORDENA VETOR DECRESCENTE ##
+	addi	t0, zero, 0 # i=0
+fori:
+	beq		t0, s7, fim_fori
+	addi	t1, zero, 0 # j=0
+	forj:
+		beq		t1, s7, fim_forj
+		slli 	t2, t1, 2 #4*j
+		add 	t2, t2, s0 #4*j + endereço base
+		addi    t3, t2, 4 #4*j + endereço base + 4 (próxima instrução)
+		lw 		t4, 0(t2) #t4=vetor[i]
+		lw 		t5, 0(t3) #t5=vetor[i+1]
+		ble		t5, t4, fim_if #if ( v[j+1]>v[j] )
+			sw t5, 0(t2) #inverte
+			sw t4, 0(t3) #inverte
+		fim_if:
+		addi	t1, t1, 1 #j++
+		jal 	zero, forj
+	fim_forj:
+	addi	t0, t0, 1 #i++
+	jal 	zero, fori
+fim_fori:
+
+
+
+fim_programa:
+	addi 	a7, zero, 10
+	ecall
